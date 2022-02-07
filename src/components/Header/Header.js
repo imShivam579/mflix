ae5b149ff1e4c01981a5e4073039e494382170f0
Dev_Shiv <im.shiv.579@gmail.com>
2022-02-07 19:38:13 +0530

    Add: input-classes

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -11,6 +11,23 @@ function Header() {
           <img src="/icon/BrandLogo-name.svg" alt="logo" typeof="image/svg" />
         </div>
       </div>
+      <div className="master__header--end">
+        <div className="input search-bar">
+          <button className="btn-right">
+            <img src="/icon/search.svg" alt="" typeof="icon/svg" />
+          </button>
+          <input type="text" placeholder="Search Movies or TV Shows" />
+          <button className="btn-left">
+            <img src="/icon/cross.svg" alt="" typeof="icon/svg" />
+          </button>
+        </div>
+        <div className="input">
+          <input type="text" placeholder="Search Movies or TV Shows" />
+          <div className="letter-count">
+            <span>0/40</span>
+          </div>
+        </div>
+      </div>
     </header>
   );
 }
