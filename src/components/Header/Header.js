340c05fb1fdc4a1f4d12877de545db575f761964
Dev_Shiv <im.shiv.579@gmail.com>
2022-02-11 20:07:23 +0530

    updating fontStyling in Btn and input class

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,10 +1,10 @@
 import React, { useEffect, useState } from "react";
 import {
-  guidBtn,
-  brandLogo,
-  search,
-  cross,
-  edit,
+  GuidBtn,
+  BrandLogo,
+  Search,
+  Cross,
+  Edit,
 } from "../../customIcons/icons";
 import "./headerStyle.css";
 function Header() {
@@ -17,22 +17,25 @@ function Header() {
     <header id="header" className="master__header bg-primary">
       <div class="master__header--wrapper">
         <div className="master__header--start">
-          <button className="btn btn__icon guide-btn fr-cc headerStart-child">
-            {guidBtn}
+          <button
+            id="guideBtn"
+            className="btn btn__icon guide-btn fr-cc headerStart-child"
+          >
+            {GuidBtn}
           </button>
-          <div className="brand-logo headerStart-child fr-cc">{brandLogo}</div>
+          <div className="brand-logo headerStart-child fr-cc">{BrandLogo}</div>
         </div>
         <div className="master__header--end">
           <button
             id="btn-feedback"
             className="btn-transparent fr-cc icon-left headerEnd-child"
           >
-            {edit}Feedback
+            {Edit}Feedback
           </button>
           <div className=" input input--searchbar headerEnd-child">
-            <button className="btn btn__icon">{search}</button>
+            <button className="btn btn__icon">{Search}</button>
             <input type="text" placeholder="Search Movies or TV Shows" />
-            <button className="btn btn__icon">{cross}</button>
+            <button className="btn btn__icon">{Cross}</button>
           </div>
           <button className="btn-fill headerEnd-child">Subscribe</button>
         </div>
